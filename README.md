007362038939bf0c75eeea0c31f4336a4dce40a8
Nicholas Joerger <nicholasajoerger@gmail.com>
2013-08-27 17:13:09 -0400

    basic test page for web, updated README

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 joergerwebsite
 ==============
+I am using Twitter's bootstrap code and do not claim to have written any of that code (included within css, js, fonts dir)
--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
+	echo('	<!DOCTYPE html>
+			<html>
+				<head>
+					<title>Nicholas Joerger as a website</title>
+					<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+				</head>
+				<body>
+					<ul class = "nav navtabs">
+						<li class = "active"></li>
+					</ul>
 
+					<div class = "row">
+					</div>
+					<script src="http://code.jquery.com/jquery.js"></script>
+					<script src="js/bootstrap.min.js"></script>
+				</body>
+			</html>
+		')
 ?>
